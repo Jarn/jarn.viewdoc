54188dfaf2e6850dad07f350f06c380fd3a2753b
Stefan H. Holek <stefan@epy.co.at>
2013-11-18 07:38:59 +0100

    Rename README.txt -> README.rst.

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ version = '2.0'
 setup(name='jarn.viewdoc',
       version=version,
       description='Python documentation viewer',
-      long_description=open('README.txt').read() + '\n' +
-                       open('CHANGES.txt').read(),
+      long_description=open('README.rst').read() + '\n' +
+                       open('CHANGES.rst').read(),
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
@@ -19,7 +19,7 @@ setup(name='jarn.viewdoc',
       keywords='view rest package docs',
       author='Stefan H. Holek',
       author_email='stefan@epy.co.at',
-      url='http://pypi.python.org/pypi/jarn.viewdoc',
+      url='https://pypi.python.org/pypi/jarn.viewdoc',
       license='BSD',
       packages=find_packages(),
       namespace_packages=['jarn'],
