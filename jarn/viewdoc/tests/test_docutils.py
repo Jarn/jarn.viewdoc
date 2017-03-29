0e3f8d1fe6d09117a139aac9e4bc65773c514dc6
Stefan H. Holek <stefan@epy.co.at>
2017-03-29 23:33:58 +0200

    Add a docutils test.

---
+++ jarn/viewdoc/tests/test_docutils.py
@@ -0,0 +1,17 @@
+import sys
+import unittest
+
+
+class DocutilsTests(unittest.TestCase):
+
+    def test_publish_string(self):
+        from docutils.core import publish_string
+
+        s = publish_string('foo', writer_name='html')
+        if sys.version_info[0] >= 3:
+            s = s.decode('utf-8')
+        self.assertTrue(s.startswith('<?xml version="1.0" encoding="utf-8" ?>'))
+
+
+def test_suite():
+    return unittest.defaultTestLoader.loadTestsFromName(__name__)
