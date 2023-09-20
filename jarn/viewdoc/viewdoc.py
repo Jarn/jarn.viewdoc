617e6b0279ce78dacf77bc39ded7f35a59aa5bfc
Stefan H. Holek <stefan@epy.co.at>
2023-09-20 08:26:45 +0200

    Support Python 3.7 via importlib_metadata.

--- jarn/viewdoc/viewdoc.py
+++ jarn/viewdoc/viewdoc.py
@@ -7,7 +7,11 @@ try:
 except locale.Error:
     pass
 
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version
+
 __version__ = version('jarn.viewdoc')
 
 import sys
