feb73ffcd340977f3940cad95e477ff8c26eed7b
Stefan H. Holek <stefan@epy.co.at>
2026-03-27 09:29:38 +0100

    Remove BBB code from tests.

--- tests/test_docutils.py
+++ tests/test_docutils.py
@@ -8,7 +8,5 @@ class DocutilsTests(unittest.TestCase):
         from docutils.core import publish_string
 
         s = publish_string('foo', writer_name='html')
-        if sys.version_info[0] >= 3:
-            s = s.decode('utf-8')
-        self.assertTrue(s.startswith('<?xml version="1.0" encoding="utf-8"'))
+        self.assertTrue(s.startswith(b'<?xml version="1.0" encoding="utf-8"'))
 
